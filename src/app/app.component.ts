e1cfb8bb3984a063264aab96f4b2330da9d99c03
Piotr <straszak.piotr@gmail.com>
2022-02-09 10:10:34 +0100

    Exercises done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  liveViewValue: string = ""
+  message: string = ""
+  spin: string = ""
+
+  zrobione() {
+    alert ('Zrobione!')
+  }
+
+  detectSpin(spin: number) {
+    this.spin = spin < 0 ? "up" : "down"
+  }
+}
